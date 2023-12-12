25229bdd8122bfd8966b82ef0cc45c2fae044a50
zivivle <zivivle@gmail.com>
2023-12-12 23:40:51 +0900

    fix: 홈페이지 key props 누락 수정

--- idearly/src/pages/HomePage/HomePage.tsx
+++ idearly/src/pages/HomePage/HomePage.tsx
@@ -5,8 +5,8 @@ import * as S from "./HomePage.styles";
 export const HomePage = () => {
   return (
     <S.HomeContainer>
-      {HomePageConfig.map((config) => (
-        <CompetitionsSection config={config} />
+      {HomePageConfig.map((config, index) => (
+        <CompetitionsSection key={index} config={config} />
       ))}
     </S.HomeContainer>
   );
