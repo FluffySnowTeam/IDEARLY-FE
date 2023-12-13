f7751e35197444e48c1b34628d78b10b6e54bdce
zivivle <zivivle@gmail.com>
2023-12-13 20:57:58 +0900

    fix: 오류 변수 주석

--- idearly/src/pages/WaitingPage/WaitingPage.tsx
+++ idearly/src/pages/WaitingPage/WaitingPage.tsx
@@ -1,11 +1,10 @@
 import { Button } from "@chakra-ui/react";
-import { useState } from "react";
 import { WaitingPageConfig } from "../../constants";
 
 export const WaitingPage = () => {
-  const [restTime, setRestTime] = useState("00:00");
+  // const [restTime, setRestTime] = useState("00:00");
   const { title, subTitle } = WaitingPageConfig;
-  const isCompetitionOpen = restTime === "00:00";
+  // const isCompetitionOpen = restTime === "00:00";
   return (
     <div>
       <div>
@@ -13,11 +12,11 @@ export const WaitingPage = () => {
         <div>{subTitle}</div>
         <div>
           <Button>주의사항</Button>
-          {isCompetitionOpen ? (
-            <Button>대회 입장</Button>
+          <Button>대회 입장</Button>
+          {/* {isCompetitionOpen ? (
           ) : (
             <Button>대회 시작까지 {restTime}</Button>
-          )}
+          )} */}
         </div>
       </div>
       <div></div>
