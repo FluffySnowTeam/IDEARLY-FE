7a3a65d6ce58f33e5278152ad7f97fd041f95fd8
KangYunJi <dbswl701@ajou.ac.kr>
2023-12-15 01:11:40 +0900

    design: 팀매칭 페이지 1차 퍼블리싱

---
+++ idearly/src/pages/TeamMatchingPage/TeamMatchingPage.styles.tsx
@@ -0,0 +1,64 @@
+import styled from "@emotion/styled";
+import { Card, CardBody, CardFooter, CardHeader, HStack, Heading, Tag, TagLabel, Text } from "@chakra-ui/react";
+
+export const TeamMathingWrapper = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const CardContainer = styled(Card)`
+  margin-top: 3rem;
+  padding: 1.5rem;
+  width: 38rem;
+`;
+
+export const CardHeaderSection = styled(CardHeader)`
+  text-align: center;
+`;
+
+export const TeamMatchingTitle = styled(Heading)`
+`;
+
+export const CardBodySection = styled(CardBody)`
+`;
+
+export const CardFooterSection = styled(CardFooter)`
+  display: flex;
+  justify-content: center;
+`;
+
+export const HStackWrapper = styled(HStack)`
+  margin-bottom: 0.7rem;
+`;
+
+export const TagLabelName = styled(TagLabel)`
+  font-size: 0.8rem;
+`;
+
+export const TagLabeEmail = styled(TagLabel)`
+  font-size: 0.6rem;
+`;
+
+export const TagWrapper = styled(Tag)`
+  background-color: #CBD5E0;
+  color: #4A5568;
+`;
+
+export const MiniTitle = styled(Text)`
+  font-weight: bold;
+  margin-bottom: 8px;
+`
+
+export const WatingWrapper = styled.div`
+  display: flex;
+  width: 60%;
+  // background-color: red;
+  justify-content: space-between;
+`;
+
+export const MemberText = styled(Text)`
+  color: #718096;
+`;
--- idearly/src/pages/TeamMatchingPage/TeamMatchingPage.tsx
+++ idearly/src/pages/TeamMatchingPage/TeamMatchingPage.tsx
@@ -1,4 +1,5 @@
-import { Button, FormControl, FormErrorMessage, FormHelperText, FormLabel, HStack, Input, Stack, Tag, TagCloseButton, TagLabel, Text } from '@chakra-ui/react'
+import { Box, Button, FormControl, FormErrorMessage, FormHelperText, FormLabel, HStack, Input, Stack, Tag, TagCloseButton, TagLabel, Text } from '@chakra-ui/react'
+import * as S from './TeamMatchingPage.styles';
 import { useState } from 'react';
 
 export const TeamMatchingPage = () => {
@@ -36,54 +37,71 @@ export const TeamMatchingPage = () => {
   ]
 
   return (
-    <>
-      <div>
-        <h1>Create a Team</h1>
-        <Input placeholder='팀 이름을 입력해주세요.' onChange={(e)=>setTeamName(e.target.value)} />
+    <S.TeamMathingWrapper>
+      <S.CardContainer>
+        <S.CardHeaderSection>
+          <S.TeamMatchingTitle>Create a Team</S.TeamMatchingTitle>
+        </S.CardHeaderSection>
 
-        <Text mb='8px'>맴버 추가</Text>
-        <HStack spacing={5}>
-          {addedMembers.map((user) => (
-            <Tag
-              size='lg'
-              key={user.id}
-              borderRadius='full'
-              variant='solid'
-              colorScheme='gray'
-            >
-              <TagLabel>{user.name}({user.email})</TagLabel>
-              <TagCloseButton />
-            </Tag>
-          ))}
-        </HStack>
-        <FormControl isInvalid={isErrorCount}>
-          <FormLabel>Email</FormLabel>
-          <Input type='email' value={input} onChange={handleInputChange} />
-          {!isErrorCount || <FormErrorMessage>Email is required.</FormErrorMessage>}
-        </FormControl>
-        <Input placeholder='검색할 맴버의 이메일 주소를 입력해주세요.' onChange={(e)=>setUserMail(e.target.value)} />
+        <S.CardBodySection>
+          <Stack spacing='8'>
+            <Box>
+              <S.MiniTitle>팀 이름</S.MiniTitle>
+              <Input placeholder='팀 이름을 입력해주세요.' onChange={(e)=>setTeamName(e.target.value)} />
+            </Box>
+            <Box>
+              <S.MiniTitle>맴버 추가</S.MiniTitle>
+              <S.HStackWrapper spacing={5}>
+                {addedMembers.map((user) => (
+                  <S.TagWrapper
+                    size='lg'
+                    key={user.id}
+                    borderRadius='full'
+                    variant='solid'
+                  >
+                    <S.TagLabelName>{user.name}</S.TagLabelName>
+                    <S.TagLabeEmail>({user.email})</S.TagLabeEmail>
+                    <TagCloseButton />
+                  </S.TagWrapper>
+                ))}
+              </S.HStackWrapper>
+              <FormControl isInvalid={isErrorCount}>
+                {/* <FormLabel>Email</FormLabel> */}
+                <Input type='email' value={input} onChange={handleInputChange} />
+                {!isErrorCount || <FormErrorMessage>Email is required.</FormErrorMessage>}
+              </FormControl>
+              <Input placeholder='검색할 맴버의 이메일 주소를 입력해주세요.' onChange={(e)=>setUserMail(e.target.value)} />
+            </Box>
+
+            <Box>
+              <S.MiniTitle>수락 대기중인 맴버</S.MiniTitle>
+              {
+                waitMembers.map((user) => {
+                  return (
+                    <S.WatingWrapper key={user.id}>
+                      <S.MemberText>{user.name}({user.email})</S.MemberText>
+                      <Button variant='link'>취소하기</Button>
+                    </S.WatingWrapper>
+                  )
+                })
+              }
+            </Box>
+
+          </Stack>
+        </S.CardBodySection>
         
-        <Text mb='8px'>수락 대기중인 맴버</Text>
-        {
-          waitMembers.map((user) => {
-            return (
-              <div style={{display: 'flex'}} key={user.id}>
-                <Text>{user.name}</Text>
-                <Text>({user.email})</Text>
-                <Button variant='link'>취소하기</Button>
-              </div>
-            )
-          })
-        }
-        <Stack direction='row' spacing={4} align='center'>
-          <Button colorScheme='teal' variant='solid'>
-            Button
-          </Button>
-          <Button colorScheme='teal' variant='outline'>
-            Button
-          </Button>
-        </Stack>
-      </div>
-    </>
+        <S.CardFooterSection>
+          <Stack direction='row' spacing={4} align='center'>
+            <Button colorScheme='facebook' variant='solid'>
+              취소하기
+            </Button>
+            <Button colorScheme='facebook' variant='outline'>
+              팀 생성하기
+            </Button>
+          </Stack>
+        </S.CardFooterSection>
+
+    </S.CardContainer>
+    </S.TeamMathingWrapper>
   );
 }
