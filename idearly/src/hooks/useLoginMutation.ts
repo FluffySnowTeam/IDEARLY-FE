785ec0dd43f2dc1f610081ff79f8541c4ea44614
zivivle <zivivle@gmail.com>
2023-12-15 20:56:57 +0900

    feat: 회원가입 로그인 성공시 로직 추가

--- idearly/src/hooks/useLoginMutation.ts
+++ idearly/src/hooks/useLoginMutation.ts
@@ -1,12 +1,18 @@
 import { useMutation } from "@tanstack/react-query";
 import { IUserRequest } from "../types";
 import { loginUser } from "../services/apis/user.apis";
+import { useNavigate } from "react-router-dom";
 
 export const useLoginMutation = () => {
+  const navigate = useNavigate();
   return useMutation({
     mutationFn: (userInfo: IUserRequest) => loginUser(userInfo),
     onError: (error) => {
       console.error(error);
+    },
+    onSuccess: (data) => {
+      navigate("/");
+      console.log(data);
     },
   });
 };
--- idearly/src/hooks/useSignupMutation.ts
+++ idearly/src/hooks/useSignupMutation.ts
@@ -4,12 +4,18 @@ import {
   registerUser,
 } from "../services/apis/user.apis";
 import { IUserSignupRequest } from "../types";
+import { useNavigate } from "react-router-dom";
 
 export const useSignupMutation = () => {
+  const navigate = useNavigate();
   return useMutation({
     mutationFn: (userInfo: IUserSignupRequest) => registerUser(userInfo),
     onError: (error) => {
       console.error(error);
+    },
+    onSuccess: (data) => {
+      navigate("/login");
+      console.log(data);
     },
   });
 };
