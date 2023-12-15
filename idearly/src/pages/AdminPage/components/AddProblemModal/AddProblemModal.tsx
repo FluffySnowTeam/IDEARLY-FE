98729a57f4c58c2c0d7fb7c3708475851ff805c4
zivivle <zivivle@gmail.com>
2023-12-15 16:20:21 +0900

    design: 모달 퍼블리싱 추가

--- idearly/src/pages/AdminPage/components/AddProblemModal/AddProblemModal.tsx
+++ idearly/src/pages/AdminPage/components/AddProblemModal/AddProblemModal.tsx
@@ -10,11 +10,18 @@ import {
 import { PropsWithChildren } from "react";
 import type { IAddProblemModal } from "./AddProblemModal.types";
 import { AddTestCase, ProblemList } from "./components";
+import { useAtomValue } from "jotai";
+import { testCaseDataAtom } from "../../../../store";
 
 export const AddProblemModal = ({
   isOpen,
   onClose,
 }: PropsWithChildren<IAddProblemModal>) => {
+  const testCaseData = useAtomValue(testCaseDataAtom);
+
+  const handleSubmit = () => {
+    console.log(testCaseData);
+  };
   return (
     <Modal isOpen={isOpen} onClose={onClose}>
       <ModalOverlay />
@@ -24,12 +31,14 @@ export const AddProblemModal = ({
 
         {/* 대회 문제리스트 */}
         <ProblemList />
-        <ModalHeader>테스크 케이스 추가</ModalHeader>
+        <ModalHeader>테스트 케이스 추가</ModalHeader>
 
         {/* 테스트케이스 관리 */}
         <AddTestCase />
         <ModalFooter>
-          <Button colorScheme="blue">수정사항 저장하기</Button>
+          <Button onClick={handleSubmit} colorScheme="blue">
+            수정사항 저장하기
+          </Button>
           <Button variant="ghost" ml={3} onClick={onClose}>
             닫기
           </Button>
--- idearly/src/pages/AdminPage/components/AdminCompetition/components/CompetitionInfoList/CompetitionInfoList.tsx
+++ idearly/src/pages/AdminPage/components/AdminCompetition/components/CompetitionInfoList/CompetitionInfoList.tsx
@@ -1,12 +1,20 @@
 import { Button, Tbody, Td, Tr } from "@chakra-ui/react";
 import { PropsWithChildren } from "react";
 import type { ICompetitionInfoList } from "./CompetitionInfoList.styles";
+import { useNavigate } from "react-router-dom";
 
 export const CompetitionInfoList = ({
   competition,
   onOpen,
 }: PropsWithChildren<ICompetitionInfoList>) => {
   const { id, title, date } = competition;
+  const navigate = useNavigate();
+
+  const handleModalOpen = () => {
+    navigate(`/admin/competition?id=${id}`);
+    onOpen();
+  };
+
   return (
     <Tbody>
       <Tr>
@@ -14,7 +22,7 @@ export const CompetitionInfoList = ({
         <Td>{title}</Td>
         <Td>{date}</Td>
         <Td>
-          <Button onClick={onOpen}>문제 수정</Button>
+          <Button onClick={handleModalOpen}>문제 수정</Button>
         </Td>
       </Tr>
     </Tbody>
