90498794b8c3bc9650a07c808b0b99ff24a881aa
KangYunJi <dbswl701@ajou.ac.kr>
2023-12-15 15:27:58 +0900

    feat: 팀 매칭의 유저 검색 기능을 디바운스로 구현

--- idearly/src/pages/TeamMatchingPage/TeamMatchingPage.tsx
+++ idearly/src/pages/TeamMatchingPage/TeamMatchingPage.tsx
@@ -1,20 +1,8 @@
 import { Box, Button, FormControl, FormErrorMessage, FormHelperText, FormLabel, HStack, Input, Stack, Tag, TagCloseButton, TagLabel, TagLeftIcon, Text } from '@chakra-ui/react'
 import * as S from './TeamMatchingPage.styles';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { AddIcon } from '@chakra-ui/icons';
 
-const waitMembers = [
-  {
-    id: 1,
-    name: '홍길동',
-    email: 'user1@example.com'
-  },
-  {
-    id: 2,
-    name: '홍길동2',
-    email: 'user2@example.com'
-  }
-]
 const addedMembers = [
   {
     id:1,
@@ -29,19 +17,35 @@ const addedMembers = [
   {
     id:3,
     name: '홍길동2',
-    email: 'user2@example.com'
+    email: 'user3@example.com'
   }
 ]
 
 export const TeamMatchingPage = () => {
   const [teamName, setTeamName] = useState<string>('');
-  const [userMail, setUserMail] = useState<string>('');
-
-  const [input, setInput] = useState('')
-
+  // const [userMail, setUserMail] = useState<string>('');
+  const [isShowUser, setIsShowUser] = useState<boolean>(false);
+  const [userInfo, setUserInfo] = useState('');
   const isErrorName = teamName === '';
   const isErrorCount = addedMembers.length !== 3; // 만약 팀 생성을 눌렀을 때, 인원이 다 안모였다면 활성화
   const isErrorTeamMatching = !isErrorName && !isErrorCount; // 팀 이름이 ''이 아니어야 하고, 맴버가 3명이어야 한다.
+
+  let timer: any;
+
+  const debounceSearch = (value: string) => {
+    clearTimeout(timer);
+    timer = setTimeout(() => {
+      // setUserMail(value);
+      // 여기에서 백엔드로 api 요청 보내기
+      if (value === 'user@example.com'){
+        setIsShowUser(true);
+        setUserInfo('홍길동(user@example.com)');
+      } else {
+        setIsShowUser(false);
+        setUserInfo('');
+      }
+    }, 400);
+  }
 
   return (
     <S.TeamMathingWrapper>
@@ -65,7 +69,7 @@ export const TeamMatchingPage = () => {
                 {addedMembers.map((user) => (
                   <S.TagWrapper
                     size='lg'
-                    key={user.id}
+                    key={user.email}
                     borderRadius='full'
                     variant='solid'
                   >
@@ -75,12 +79,21 @@ export const TeamMatchingPage = () => {
                   </S.TagWrapper>
                 ))}
               </S.HStackWrapper>
-              <Input type='email' placeholder='검색할 맴버의 이메일 주소를 입력해주세요.' onChange={(e)=>setUserMail(e.target.value)} />
+              <Input 
+                type='email' 
+                placeholder='검색할 맴버의 이메일 주소를 입력해주세요.' 
+                onChange={(e)=>debounceSearch(e.target.value)}
+              />
               {/* 유저 보여주는데 버튼을 사용하는게 적절할까요? 처음에는 Tag를 사용해볼까 했는데... */}
-              <S.ShowUserBtn colorScheme='gray'>
-                <TagLeftIcon boxSize='12px' as={AddIcon} />
-                홍길동(user@example.com)
-              </S.ShowUserBtn>
+              {
+                isShowUser
+                  &&
+                <S.ShowUserBtn colorScheme='gray'>
+                  <TagLeftIcon boxSize='12px' as={AddIcon} />
+                  {userInfo}
+                </S.ShowUserBtn>
+              }
+
             </Box>
 
           </Stack>
