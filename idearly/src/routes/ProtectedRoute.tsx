7fbdd253255b6f79dfd3d337b636011a845908bc
zivivle <zivivle@gmail.com>
2023-12-16 17:32:07 +0900

    fix: 리다이렉션 로직 수정

--- idearly/src/routes/ProtectedRoute.tsx
+++ idearly/src/routes/ProtectedRoute.tsx
@@ -1,9 +1,30 @@
-import Cookies from "js-cookie";
-import { Navigate, Outlet } from "react-router-dom";
+import axios from "axios";
+import { useEffect } from "react";
+import { Outlet, useNavigate } from "react-router-dom";
 
 const ProtectedRoute = () => {
-  const accessToken = Cookies.get("accessToken");
-  return accessToken ? <Outlet /> : <Navigate to="/login" />;
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    // 인터셉터 설정
+    const axiosInterceptor = axios.interceptors.response.use(
+      (response) => response,
+      (error) => {
+        if (error.response && error.response.status === 401) {
+          // 인증 오류가 발생하면 로그인 페이지로 리다이렉션
+          navigate("/login");
+        }
+        return Promise.reject(error);
+      }
+    );
+
+    // 컴포넌트가 언마운트될 때 인터셉터 제거
+    return () => {
+      axios.interceptors.response.eject(axiosInterceptor);
+    };
+  }, [navigate]);
+
+  return <Outlet />;
 };
 
 export default ProtectedRoute;
