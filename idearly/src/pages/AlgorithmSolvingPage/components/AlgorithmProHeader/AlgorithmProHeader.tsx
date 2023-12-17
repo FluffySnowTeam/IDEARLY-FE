521a026808b8b57c5d264887fc6b481abe022ddd
죠 <stop.ho@khu.ac.kr>
2023-12-18 03:18:20 +0900

    design: 언어 선택 default Python으로 변경

--- idearly/src/pages/AlgorithmSolvingPage/components/AlgorithmProHeader/AlgorithmProHeader.tsx
+++ idearly/src/pages/AlgorithmSolvingPage/components/AlgorithmProHeader/AlgorithmProHeader.tsx
@@ -1,13 +1,16 @@
-import * as S from "./AlgorithmProHeader.styles";
+import * as S from './AlgorithmProHeader.styles';
 
 export const AlgorithmProHeader = () => {
   return (
     <S.AlgorithmContainer>
       <div>
-        <S.AlgorithmLanguage placeholder="언어 선택">
-          <option value="Python">Python </option>
-          <option value="Java">Java </option>
-          <option value="JavaScript">JavaScript </option>
+        <S.AlgorithmLanguage>
+          <option value='none'>언어 선택</option>
+          <option value='Python' selected>
+            Python
+          </option>
+          <option value='Java'>Java</option>
+          <option value='JavaScript'>JavaScript</option>
         </S.AlgorithmLanguage>
       </div>
     </S.AlgorithmContainer>
