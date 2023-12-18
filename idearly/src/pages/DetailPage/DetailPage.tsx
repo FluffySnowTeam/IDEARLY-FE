369d14bf362be028696cec70dd726abce30c5d58
zivivle <zivivle@gmail.com>
2023-12-18 22:53:25 +0900

    design: 상세 페이지 퍼블리싱 완료
    목데이터 데이터 형식 수정

--- idearly/src/pages/DetailPage/DetailPage.tsx
+++ idearly/src/pages/DetailPage/DetailPage.tsx
@@ -1,3 +1,38 @@
+import { useNavigate, useParams } from "react-router-dom";
+import { fakeCompetitions } from "../../mocks/competition.mocks";
+import { Button } from "@chakra-ui/react";
+import * as S from "./DetailPage.styles";
+import { dateChange } from "../../utils/dateChange";
+
 export const DetailPage = () => {
-  return <div>DetailPage</div>
-}
+  const { id } = useParams<{ id: string }>();
+  const navigate = useNavigate();
+  const selectCompetition = fakeCompetitions.filter(
+    (competition) => competition.competitionId === id
+  );
+  const { title, description, startDateTime, endDateTime, participate } =
+    selectCompetition[0];
+
+  const handleMoveToPage = () => {
+    if (participate) {
+      navigate(`/waiting/${id}`);
+    } else {
+      navigate(`/matching`);
+    }
+  };
+  return (
+    <S.CompetitionDetailContainer>
+      <S.CompeDetailTitle>{title}</S.CompeDetailTitle>
+      <S.CompeDetailDate>
+        <div>시작: {dateChange({ date: startDateTime })}</div>
+        <div>종료: {dateChange({ date: endDateTime })}</div>
+      </S.CompeDetailDate>
+      <S.CompeDetailDescription
+        dangerouslySetInnerHTML={{
+          __html: description.replace(/\n/g, "<br />"),
+        }}
+      ></S.CompeDetailDescription>
+      <Button onClick={handleMoveToPage}>대회 참여하기</Button>
+    </S.CompetitionDetailContainer>
+  );
+};
