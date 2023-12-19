877907844393efdac42fde02b410ee7c4c08493f
zivivle <zivivle@gmail.com>
2023-12-19 14:28:55 +0900

    feat: 초기화 로직 추가

--- idearly/src/pages/AdminPage/components/AddProblemModal/AddProblemModal.tsx
+++ idearly/src/pages/AdminPage/components/AddProblemModal/AddProblemModal.tsx
@@ -10,14 +10,19 @@ import {
 import { PropsWithChildren } from "react";
 import type { IAddProblemModal } from "./AddProblemModal.types";
 import { AddTestCase, ProblemList } from "./components";
-import { useAtomValue } from "jotai";
+import { useAtom } from "jotai";
 import { testCaseDataAtom } from "../../../../store";
 
 export const AddProblemModal = ({
   isOpen,
   onClose,
 }: PropsWithChildren<IAddProblemModal>) => {
-  const testCaseData = useAtomValue(testCaseDataAtom);
+  const [testCaseData, setTestCaseList] = useAtom(testCaseDataAtom);
+
+  const handleClose = () => {
+    setTestCaseList([]); // testCaseDataAtom 초기화
+    onClose(); // 원래의 onClose 함수 호출
+  };
 
   const handleSubmit = () => {
     console.log(testCaseData);
@@ -27,7 +32,7 @@ export const AddProblemModal = ({
       <ModalOverlay />
       <ModalContent>
         <ModalHeader>문제 리스트</ModalHeader>
-        <ModalCloseButton />
+        <ModalCloseButton onClick={handleClose} />
 
         {/* 대회 문제리스트 */}
         <ProblemList />
@@ -39,7 +44,7 @@ export const AddProblemModal = ({
           <Button onClick={handleSubmit} colorScheme="blue">
             수정사항 저장하기
           </Button>
-          <Button variant="ghost" ml={3} onClick={onClose}>
+          <Button variant="ghost" ml={3} onClick={handleClose}>
             닫기
           </Button>
         </ModalFooter>
--- idearly/src/pages/AdminPage/components/AdminCompetition/components/CompetitionInfoList/CompetitionInfoList.tsx
+++ idearly/src/pages/AdminPage/components/AdminCompetition/components/CompetitionInfoList/CompetitionInfoList.tsx
@@ -2,25 +2,29 @@ import { Button, Tbody, Td, Tr } from "@chakra-ui/react";
 import { PropsWithChildren } from "react";
 import type { ICompetitionInfoList } from "./CompetitionInfoList.styles";
 import { useNavigate } from "react-router-dom";
+import { dateChange } from "../../../../../../utils/dateChange";
 
 export const CompetitionInfoList = ({
   competition,
   onOpen,
 }: PropsWithChildren<ICompetitionInfoList>) => {
-  const { id, title, date } = competition;
+  const { competitionId, title, startDateTime, endDateTime } = competition;
   const navigate = useNavigate();
 
   const handleModalOpen = () => {
-    navigate(`/admin/competition?id=${id}`);
+    navigate(`/admin/competition?id=${competitionId}`);
     onOpen();
   };
 
   return (
     <Tbody>
       <Tr>
-        <Td>{id}</Td>
+        <Td>{competitionId}</Td>
         <Td>{title}</Td>
-        <Td>{date}</Td>
+        <Td>
+          {dateChange({ date: startDateTime })} ~
+          {dateChange({ date: endDateTime })}
+        </Td>
         <Td>
           <Button onClick={handleModalOpen}>문제 수정</Button>
         </Td>
