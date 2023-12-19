979536b7972da2168d20731bd197c4fc6499e0e3
KangYunJi <dbswl701@ajou.ac.kr>
2023-12-19 14:11:26 +0900

    feat: 마이 페이지 현재 참가 소속팀 구현

--- idearly/src/pages/MyPage/components/MyPgaeSearchTeam/MyPageSearchTeam.tsx
+++ idearly/src/pages/MyPage/components/MyPgaeSearchTeam/MyPageSearchTeam.tsx
@@ -1,5 +1,45 @@
+import { Button, Table, TableContainer, Tbody, Td, Th, Thead, Tr } from "@chakra-ui/react";
+import * as S from "./MyPageSearchTema.styles";
+import { curCompetition } from "../../../../mocks/curCompetition.mocks";
+
 export const MyPageSearchTeam = () => {
   return (
-    <div>MyPageSearchTeam</div>
+    <S.SearchTeamWrapper>
+      <S.SearchTeamTitle>현재 팀 조회</S.SearchTeamTitle>
+      <S.SearchTeamSubTitle>참가 대회 소속팀</S.SearchTeamSubTitle>
+      <S.SearchTeamTableContainer>
+        <Table variant='simple'>
+          <Thead>
+            <Tr>
+              <Th>참가 대회</Th>
+              <Th>팀명</Th>
+              <Th>팀장</Th>
+              <Th>대회 시작 날짜</Th>
+              <Th>팀 관리</Th>
+            </Tr>
+          </Thead>
+          <Tbody>
+            {
+              curCompetition.map((competiton) => {
+                return (
+                  <Tr key={competiton.competitionId}>
+                    <Td>{competiton.competitionTitle}</Td>
+                    <Td>{competiton.teamName}</Td>
+                    <Td>{competiton.leaderName}</Td>
+                    <Td>{competiton.startDateTime}</Td>
+                    <Td>
+                      <Button>상세 보기</Button>  
+                    </Td>
+                  </Tr>
+                )
+              })
+            }
+          </Tbody>
+        </Table>
+      </S.SearchTeamTableContainer>
+
+
+      <S.SearchTeamSubTitle>대기 중인 초대 현황</S.SearchTeamSubTitle>
+    </S.SearchTeamWrapper>
   )
 }
---
+++ idearly/src/pages/MyPage/components/MyPgaeSearchTeam/MyPageSearchTema.styles.tsx
@@ -0,0 +1,33 @@
+import { TableContainer } from "@chakra-ui/react";
+import styled from "@emotion/styled";
+
+export const SearchTeamWrapper = styled.div`
+  width: 100%;
+  // background-color: red;
+  display: flex;
+  flex-direction: column;
+  // justify-content: center;
+  // align-items: center;
+  // padding-top: 4rem;
+  // // padding-left: 5rem;
+  padding: 4rem 5rem;
+`;
+
+export const SearchTeamTitle = styled.div`
+  font-size: 2rem;
+  font-weight: bold;
+  text-align: center;
+`;
+
+export const SearchTeamSubTitle = styled.div`
+  font-size: 1.2rem;
+  font-weight: 600;
+  margin-top: 2rem;
+`;
+
+export const SearchTeamTableContainer = styled(TableContainer)`
+  border: 1px solid #cccccc;
+  border-radius: 1rem;
+  padding: 0.5rem;
+  margin-top: 1rem;
+`;
