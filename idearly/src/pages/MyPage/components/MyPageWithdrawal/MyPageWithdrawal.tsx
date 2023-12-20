5d89eca4a4525f0045ba58b27688018d8061a452
KangYunJi <dbswl701@ajou.ac.kr>
2023-12-20 12:21:30 +0900

    refactor: 마이페이지 회원탈퇴 상수들 constant 파일로 관리

--- idearly/src/pages/MyPage/components/MyPageWithdrawal/MyPageWithdrawal.tsx
+++ idearly/src/pages/MyPage/components/MyPageWithdrawal/MyPageWithdrawal.tsx
@@ -1,30 +1,30 @@
+import { MyPageWithdrawalConfig } from "../../../../constants/MyPage.constants";
 import * as S from "./MyPageWithdrawal.styles";
 import { useState } from "react";
 
 export const MyPageWithdrawal = () => {
   const [isCheck, setIsCheck] = useState(false);
-
+  const {title1, content1, title2, content2, title3, content3, title4, title5, content5} = MyPageWithdrawalConfig;
+  
   return (
     <S.WithdrawalContainer>
       <S.WithdrawalTitle>회원 탈퇴</S.WithdrawalTitle>
-      <S.WithdrawalSubTitle>탈퇴하시기 전에</S.WithdrawalSubTitle>
-      <S.WithdrawalText>- 탈퇴 시 본 서비스에 등록한 모든 정보가 영구적으로 삭제되며, 재가입 또는 복구가 불가합니다.</S.WithdrawalText>
+      <S.WithdrawalSubTitle>{title1}</S.WithdrawalSubTitle>
+      <S.WithdrawalText>{content1}</S.WithdrawalText>
 
-      <S.WithdrawalSubTitle>미리 백업하기</S.WithdrawalSubTitle>
-      <S.WithdrawalText>- 등록한 서비스에서 정보 백업을 원하실 경우, 탈퇴 이전에 서비스에서 백업을 진행해주세요. 탈퇴 후에는 데이터를 복구할 수 없습니다.</S.WithdrawalText>
+      <S.WithdrawalSubTitle>{title2}</S.WithdrawalSubTitle>
+      <S.WithdrawalText>{content2}</S.WithdrawalText>
 
-      <S.WithdrawalSubTitle>미리 관리하기</S.WithdrawalSubTitle>
-      <S.WithdrawalText>- 서비스 이용 중 본인의 계정에 귀속되지 않은 정보는 자동으로 삭제되지 않으며, 탈퇴 시 수정이나 삭제가 불가능합니다. 본인 계정에 귀속되지 않은 정보를 수정하거나 삭제하려는 경우, 회원탈퇴 이전에 해당 서비스에서 수정 또는 삭제를 진행해주세요.</S.WithdrawalText>
+      <S.WithdrawalSubTitle>{title3}</S.WithdrawalSubTitle>
+      <S.WithdrawalText>{content3}</S.WithdrawalText>
 
-      <S.WithdrawalSubTitle>탈퇴하려는 계정</S.WithdrawalSubTitle>
+      <S.WithdrawalSubTitle>{title4}</S.WithdrawalSubTitle>
       <S.UserTag size="lg" variant='solid' colorScheme='gray'>
         강윤지(user@example.com)
       </S.UserTag>
 
-      <S.WithdrawalSubTitle>삭제되는 정보</S.WithdrawalSubTitle>
-      <S.WithdrawalText>- 활동 데이터</S.WithdrawalText>
-      <S.WithdrawalText>- 기록 데이터</S.WithdrawalText>
-      <S.WithdrawalText>- 개인정보 데이터</S.WithdrawalText>
+      <S.WithdrawalSubTitle>{title5}</S.WithdrawalSubTitle>
+      <S.WithdrawalText>{content5}</S.WithdrawalText>
       <S.RadioIcon
         className="material-icons"
         onClick={() => setIsCheck(prev => !prev)}
