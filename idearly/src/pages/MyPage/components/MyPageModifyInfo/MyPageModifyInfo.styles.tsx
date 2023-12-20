e2c320638dc12e58b2c5af95c59513b639a20bfc
KangYunJi <dbswl701@ajou.ac.kr>
2023-12-20 11:28:08 +0900

    design: 마이페이지 - 내 정보 수정 퍼블리싱

---
+++ idearly/src/pages/MyPage/components/MyPageModifyInfo/MyPageModifyInfo.styles.tsx
@@ -0,0 +1,13 @@
+import { Card, Input } from "@chakra-ui/react";
+import styled from "@emotion/styled";
+
+export const CardWrapper = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const ModifyInput = styled(Input)`
+  width: 300px;
+`;
--- idearly/src/pages/MyPage/components/MyPageModifyInfo/MyPageModifyInfo.tsx
+++ idearly/src/pages/MyPage/components/MyPageModifyInfo/MyPageModifyInfo.tsx
@@ -1,6 +1,21 @@
+import { Button, Card, CardBody, CardFooter, CardHeader, Heading, Input, Text } from "@chakra-ui/react"
+import * as S from "./MyPageModifyInfo.styles";
+
 export const MyPageModifyInfo = () => {
   return (
-    <div>MyPageModifyInfo</div>
+    <S.CardWrapper>
+      <Card align='center'>
+        <CardHeader>
+          <Heading size='md'> 이름 변경</Heading>
+        </CardHeader>
+        <CardBody>
+          <S.ModifyInput placeholder='변경할 이름을 입력해주세요.' />
+        </CardBody>
+        <CardFooter>
+          <Button colorScheme='blue'>수정</Button>
+        </CardFooter>
+      </Card>
+    </S.CardWrapper>
   )
 }
 
