2777969c5bdaf0bc950435c8cf519e492d18e29f
KangYunJi <dbswl701@ajou.ac.kr>
2023-12-20 17:47:21 +0900

    feat: 마이페이지 팀장이 팀원 추가하는 기능 구현

---
+++ idearly/src/pages/MyPage/components/MyPgaeCurrentTeam/components/TeamModifyModal/TeamModifyModal.styles.tsx
@@ -0,0 +1,25 @@
+import { Icon } from "@chakra-ui/react";
+import styled from "@emotion/styled";
+
+export const ModalSubTitle = styled.div`
+  font-weight: bold;
+  font-size: 1.2rem;
+  margin-top: 1.5rem;
+`;
+
+export const ModalContent = styled.div`
+  color: gray;
+  padding-left: 0.5rem;
+`;
+
+export const MemberWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  width: 13rem;
+  align-items: center;
+`;
+
+export const IconWrapper = styled(Icon)`
+  color: gray;
+  cursor: pointer;
+`;
--- idearly/src/pages/MyPage/components/MyPgaeCurrentTeam/components/TeamModifyModal/TeamModifyModal.tsx
+++ idearly/src/pages/MyPage/components/MyPgaeCurrentTeam/components/TeamModifyModal/TeamModifyModal.tsx
@@ -1,8 +1,25 @@
-import { Button, Modal, ModalBody, ModalCloseButton, ModalContent, ModalFooter, ModalHeader, ModalOverlay } from "@chakra-ui/react"
-import { PropsWithChildren } from "react";
+import { Button, Icon, Modal, ModalBody, ModalCloseButton, ModalContent, ModalFooter, ModalHeader, ModalOverlay, TagCloseButton } from "@chakra-ui/react"
+import { PropsWithChildren, useState } from "react";
 import { ITeamlModal } from "../../MyPageCurrentTeam.types";
+import { AddTeamMembers } from "../../../../../../components/AddTeamMembers/AddTeamMembers";
+import { IUserType } from "../../../../../TeamMatchingPage/TeamMatchingPage.types";
+import * as S from "./TeamModifyModal.styles";
+import { CloseIcon } from "@chakra-ui/icons";
 
 export const TeamModifyModal = ({ isOpen, onClose, currentMemberList, inviteMemberList}: PropsWithChildren<ITeamlModal>) => {
+  const [addedMembers, setAddedMembers] = useState<IUserType[]>(currentMemberList);
+  const MAX_MEMBER = 2;
+  const isErrorCount = addedMembers.length !== MAX_MEMBER; // 만약 팀 생성을 눌렀을 때, 인원이 다 안모였다면 활성화
+
+  const [addedInviteMembers, setAddedInviteMembers] = useState(inviteMemberList);
+  
+  console.log('cur: ', addedMembers);
+  console.log('invite: ', addedInviteMembers);
+  
+  const handleCancleMember = (email: string) => {
+    // 해당 email을 가지고 있는 유저 삭제
+    setAddedInviteMembers(addedInviteMembers.filter(member => member.email !== email));
+  }
   return (
     <Modal isOpen={isOpen} onClose={onClose}>
         <ModalOverlay />
@@ -10,12 +27,23 @@ export const TeamModifyModal = ({ isOpen, onClose, currentMemberList, inviteMemb
           <ModalHeader>팀 상세 정보</ModalHeader>
           <ModalCloseButton />
           <ModalBody>
+            <S.ModalSubTitle>맴버 추가(2/3)</S.ModalSubTitle>
+            <AddTeamMembers addedMembers={addedMembers} setAddedMembers={setAddedMembers} isErrorCount={isErrorCount} />
+            <S.ModalSubTitle>수락 대기중인 맴버</S.ModalSubTitle>
+            {
+              addedInviteMembers.map(member => (
+                <S.MemberWrapper key={member.email}>
+                  <S.ModalContent key={member.email}>{member.name}({member.email})</S.ModalContent>
+                  <S.IconWrapper as={CloseIcon} onClick={() => handleCancleMember(member.email)} />
+                </S.MemberWrapper>
+              ))
+            }
+            <div style={{display: 'flex', justifyContent: 'flex-end'}}>
+              <Button colorScheme='blue' onClick={onClose} >
+                수정
+              </Button>
+            </div>
           </ModalBody>
-          <ModalFooter>
-            <Button colorScheme='blue' onClick={onClose}>
-              Close
-            </Button>
-          </ModalFooter>
         </ModalContent>
       </Modal>
   )
