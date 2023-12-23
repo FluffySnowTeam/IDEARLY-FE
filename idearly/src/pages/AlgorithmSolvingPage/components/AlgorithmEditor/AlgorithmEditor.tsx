bb1b326549dde799fc317509b9fa113f29f4d324
죠 <stop.ho@khu.ac.kr>
2023-12-23 18:24:26 +0900

    design: 코드에디터 크기 조정, 언어 python으로 설정

--- idearly/src/pages/AlgorithmSolvingPage/components/AlgorithmEditor/AlgorithmEditor.tsx
+++ idearly/src/pages/AlgorithmSolvingPage/components/AlgorithmEditor/AlgorithmEditor.tsx
@@ -1,9 +1,16 @@
 import * as S from "./AlgorithmEditor.styles";
+import CodeMirrorComponent from "./[CY]AlgorithmEditor";
+import CodeMirror from "@uiw/react-codemirror";
+import { javascript } from "@codemirror/lang-javascript";
+import { python } from "@codemirror/lang-python";
+import { java } from "@codemirror/lang-java";
+import { githubDark, githubLight } from "@uiw/codemirror-theme-github";
 
 export const AlgorithmEditor = () => {
   return (
     <S.AlgorithmEditorContainer>
-      <div></div>
+      {/* <CodeMirror theme={githubLight} extensions={[python()]} height="64.9vh" /> */}
+      <CodeMirrorComponent />
     </S.AlgorithmEditorContainer>
   );
 };
