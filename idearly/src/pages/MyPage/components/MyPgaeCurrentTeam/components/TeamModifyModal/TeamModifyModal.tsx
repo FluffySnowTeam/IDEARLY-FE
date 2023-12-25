c9fdbe0730c48f8d68ad9742d2b1275a468a61fc
KangYunJi <dbswl701@ajou.ac.kr>
2023-12-25 18:49:23 +0900

    fix: 팀원수 못채우면 수정 못하도록 수정

--- idearly/src/pages/MyPage/components/MyPgaeCurrentTeam/components/TeamModifyModal/TeamModifyModal.tsx
+++ idearly/src/pages/MyPage/components/MyPgaeCurrentTeam/components/TeamModifyModal/TeamModifyModal.tsx
@@ -12,32 +12,28 @@ import { userInfoAtom } from "../../../../../../store";
 
 export const TeamModifyModal = ({ isOpen, onClose, currentMemberList, inviteMemberList, setCurrentMemberList, setInviteMemberList, teamId}: PropsWithChildren<IModifyTeamlModal>) => {
 
-  const MAX_MEMBER = 2;
-  const isErrorCount = currentMemberList?.length !== MAX_MEMBER; // 만약 팀 생성을 눌렀을 때, 인원이 다 안모였다면 활성화
-  const { mutate } = useModifyTeamMembersMutation();
-  console.log('In modify modal, cur:', currentMemberList, 'invite:', inviteMemberList);
+  const MAX_MEMBER = 3;
   const userInfo = useAtomValue(userInfoAtom);
+  const { mutate } = useModifyTeamMembersMutation();
+  const totalMemberNum = currentMemberList?.length + inviteMemberList?.length;
+  const isFullMember = totalMemberNum !== MAX_MEMBER; // 만약 팀 생성을 눌렀을 때, 인원이 다 안모였다면 활성화
 
   const handleDropoutMember = (email: string) => {
-    // 해당 email을 가지고 있는 유저 삭제
-    setInviteMemberList(inviteMemberList.filter(member => member.email !== email));
+    setCurrentMemberList(currentMemberList.filter(member => member.email !== email));
   }
 
   const handleDelete = (email: string) => {
-    setCurrentMemberList(currentMemberList.filter((user) => user.email !== email));
+    console.log(email);
+    setInviteMemberList(inviteMemberList.filter((user) => user.email !== email));
   }
 
   const handleSubmit = () => {
-    onClose();
-
     const temp: ITeamMember[] = [...inviteMemberList, ...currentMemberList]
-
     const payload: IReqTeamMember[] = temp.map((member) => ({ name: member.name, email: member.email })).filter(member => member.email !== userInfo.email);
-
-    console.log(payload);
-    
     mutate({teamId, payload});
+    onClose();
   }
+  console.log('In modify modal, currentMemberList: ', currentMemberList, 'inviteMemberList:', inviteMemberList);
 
   return (
     <Modal isOpen={isOpen} onClose={onClose}>
@@ -46,27 +42,29 @@ export const TeamModifyModal = ({ isOpen, onClose, currentMemberList, inviteMemb
           <ModalHeader>팀 상세 정보</ModalHeader>
           <ModalCloseButton />
           <ModalBody>
-            <S.ModalSubTitle>총 맴버 수({currentMemberList?.length+inviteMemberList?.length + 1}/{MAX_MEMBER+1})</S.ModalSubTitle>
-            <AddTeamMembers setAddedMembers={setCurrentMemberList} isErrorCount={isErrorCount} />
+            <S.ModalSubTitle>총 맴버 수({totalMemberNum}/{MAX_MEMBER})</S.ModalSubTitle>
+            <AddTeamMembers setAddedMembers={setInviteMemberList} isErrorCount={isFullMember} />
             <S.ModalSubTitle>수락 대기중인 맴버</S.ModalSubTitle>
 
             <S.MemberListWrapper>
-              {currentMemberList?.map((user: IUserType) => (
-                <S.MemberWrapper key={user.email}>
-                  <S.ModalContent key={user.email}>{user.name}({user.email})</S.ModalContent>
-                  <S.IconWrapper as={CloseIcon} onClick={() => handleDelete(user.email)} />
-                </S.MemberWrapper>
-              ))}
+              {
+                inviteMemberList?.map((user: IUserType) => (
+                  <S.MemberWrapper key={user.email}>
+                    <S.ModalContent key={user.email}>{user.name}({user.email})</S.ModalContent>
+                    <S.IconWrapper as={CloseIcon} onClick={() => handleDelete(user.email)} />
+                  </S.MemberWrapper>
+                ))
+              }
             </S.MemberListWrapper>
             
               
-            <S.ModalSubTitle>수락완료한 맴버</S.ModalSubTitle>
-            <S.ModalContent>
+            <S.ModalSubTitle>맴버 리스트</S.ModalSubTitle>
+              <S.ModalContent>
                 나
               </S.ModalContent>
             <S.MemberListWrapper>
             {
-              inviteMemberList?.map(member => (
+              currentMemberList?.filter(member => member.email !== userInfo.email).map(member => (
                 <S.MemberWrapper key={member.email}>
                   <S.ModalContent key={member.email}>{member.name}({member.email})</S.ModalContent>
                   <S.Dropout onClick={() => handleDropoutMember(member.email)}>강퇴</S.Dropout>
@@ -76,7 +74,7 @@ export const TeamModifyModal = ({ isOpen, onClose, currentMemberList, inviteMemb
             </S.MemberListWrapper>
           </ModalBody>
           <ModalFooter>
-            <Button colorScheme='blue' onClick={handleSubmit} >
+            <Button colorScheme='blue' onClick={handleSubmit} isDisabled={isFullMember}>
               수정
             </Button>
           </ModalFooter>
