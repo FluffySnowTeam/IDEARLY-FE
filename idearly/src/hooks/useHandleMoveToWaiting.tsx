79010f22620a21fb7c4edd1e20b134829d1d8504
zivivle <zivivle@gmail.com>
2023-12-26 17:10:51 +0900

    remove: 메인 참가하기 버튼 삭제

--- idearly/src/hooks/useHandleMoveToWaiting.tsx
+++ idearly/src/hooks/useHandleMoveToWaiting.tsx
@@ -1,5 +1,4 @@
-import { ModalOverlay, useDisclosure } from "@chakra-ui/react";
-import { useCallback, useState } from "react";
+import { useCallback } from "react";
 import { useNavigate } from "react-router-dom";
 import type { ICompetition } from "../types";
 
@@ -18,38 +17,16 @@ const useHandleMoveToWaiting = (competition?: ICompetition) => {
   const { competitionId, startDateTime, participate } =
     competition || defaultCompetition;
   const navigate = useNavigate();
-  const { isOpen, onOpen, onClose } = useDisclosure();
-  const OverlayOne = () => (
-    <ModalOverlay
-      bg="blackAlpha.300"
-      backdropFilter="blur(10px) hue-rotate(90deg)"
-    />
-  );
-  const [overlay, setOverlay] = useState(<OverlayOne />);
+
   const handleMoveToWaiting = useCallback(() => {
-    // 만약 대회에 소속된 팀이 있다면
-    const now = new Date();
-    const startDate = new Date(startDateTime);
-
-    now.setHours(0, 0, 0, 0);
-    startDate.setHours(0, 0, 0, 0);
-
-    if (startDate.getTime() === now.getTime()) {
-      // 만약 대회 날짜가 오늘이면
-      navigate(`/waiting/${competitionId}`);
-    } else if (participate === false) {
+    if (participate === false) {
       navigate(`/matching/${competitionId}`);
     } else {
-      // 대회 날짜가 오늘이 아니면 모달 표시
-      onOpen();
-      setOverlay(<OverlayOne />);
+      navigate(`/waiting/${competitionId}`);
     }
+  }, [navigate, competitionId, startDateTime, participate]);
 
-    // 만약 대회에 소속된 팀이 없다면?
-    // navigate(`/matching`);
-  }, [navigate, competitionId, startDateTime, participate, onOpen, setOverlay]);
-
-  return { isOpen, onClose, overlay, handleMoveToWaiting };
+  return { handleMoveToWaiting };
 };
 
 export default useHandleMoveToWaiting;
--- idearly/src/pages/HomePage/components/CompetitionsCard/CompetitionsCard.tsx
+++ idearly/src/pages/HomePage/components/CompetitionsCard/CompetitionsCard.tsx
@@ -2,17 +2,12 @@ import { PropsWithChildren } from "react";
 import * as S from "./CompetitionCard.styles";
 import type { ICompetitionCard } from "./CompetitionsCard.types";
 import { useNavigate } from "react-router-dom";
-import { CompetitionsModal } from "../CompetitionsModal/CompetitionsModal";
 import { dateChange } from "../../../../utils/dateChange";
-import useHandleMoveToWaiting from "../../../../hooks/useHandleMoveToWaiting";
 
 export const CompetitionsCard = ({
   competition,
 }: PropsWithChildren<ICompetitionCard>) => {
   const navigate = useNavigate();
-
-  const { isOpen, onClose, overlay, handleMoveToWaiting } =
-    useHandleMoveToWaiting(competition);
 
   // 대회 데이터 아이디로 수정해야함
   const handleMoveToDetail = () => {
@@ -21,12 +16,6 @@ export const CompetitionsCard = ({
 
   return (
     <>
-      <CompetitionsModal
-        isOpen={isOpen}
-        onClose={onClose}
-        overlay={overlay}
-        startDateTime={competition.startDateTime}
-      />
       <S.CompeCardContainer
         spacing={6}
         templateColumns="repeat(auto-fill, minmax(200px, 1fr))"
@@ -44,11 +33,8 @@ export const CompetitionsCard = ({
               대회 종료: {dateChange({ date: competition?.endDateTime })}
             </S.CompeText>
             <S.CardDetailButton onClick={handleMoveToDetail}>
-              대회 상세보기
+              대회 상세보기 및 참가하기
             </S.CardDetailButton>
-            <S.CompeCardButton onClick={handleMoveToWaiting}>
-              대회 참가하기
-            </S.CompeCardButton>
           </S.CompeCardBody>
         </S.CompeCardBox>
       </S.CompeCardContainer>
