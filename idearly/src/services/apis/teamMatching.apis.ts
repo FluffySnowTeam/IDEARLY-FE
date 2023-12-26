bcd8d0dcae9746602fd7ed6e770e016350f5c98a
zivivle <zivivle@gmail.com>
2023-12-26 15:04:06 +0900

    fix: merge to main

--- idearly/src/services/apis/teamMatching.apis.ts
+++ idearly/src/services/apis/teamMatching.apis.ts
@@ -2,16 +2,10 @@ import { axiosInstance } from "./axios";
 
 // 팀 생성 요청
 export const teamCreate = async (competitionId: string, payload: any) => {
-  return await axiosInstance.post(
-    `/api/competitions/${competitionId}`,
-    payload
-  );
-};
+  return await axiosInstance.post(`/api/competitions/${competitionId}`, payload);
+}
 
 // 이메일로 회원 조회 -> 이 코드 user.api.ts로 옮기는게 좋을까요?
 export const searchMember = async (competitionId: string, email: string) => {
-  const response = await axiosInstance.get(
-    `/api/competitions/${competitionId}/members?email=${email}`
-  );
-  return response.data;
-};
+  return await axiosInstance.get(`/api/competitions/${competitionId}/members?email=${email}`);
+}
