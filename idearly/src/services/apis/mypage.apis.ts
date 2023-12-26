a6fd194457cac0951de88f3404a4c0b13b8cb7b0
KangYunJi <dbswl701@ajou.ac.kr>
2023-12-26 12:59:25 +0900

    fix: 회원 정보 수정 응답 형식 data에서 result로 변경

--- idearly/src/services/apis/mypage.apis.ts
+++ idearly/src/services/apis/mypage.apis.ts
@@ -3,40 +3,43 @@ import { axiosInstance } from "./axios";
 // 회원 탈퇴 요청
 export const withdrawalUser = async () => {
   return await axiosInstance.delete("/api/members");
-}
+};
 
 // 회원 정보 수정 요청
 export const modifyUser = async (name: string) => {
-  return await axiosInstance.patch("/api/members", {name});
-}
+  const response = await axiosInstance.patch("/api/members", { name });
+  return response.data;
+};
 
 // 현재 팀 조회 - 참가 대회 소속팀 조회 요청
 export const getCurrentTeam = async () => {
   const response = await axiosInstance.get("/api/teams?invite=true");
   return response.data;
-}
+};
 
 // 현재 팀 조회 - 대기중인 초대 현황 조회 요청
 export const getCWaitTeam = async () => {
   const response = await axiosInstance.get("/api/teams?invite=false");
   return response.data;
-}
+};
 
 // 팀 초대 요청 수락 / 거절
-export const handleInvite = async (teamId:number, isAccept: boolean) => {
-  const resposne = await axiosInstance.post(`/api/teams/${teamId}`, {accept: isAccept});
+export const handleInvite = async (teamId: number, isAccept: boolean) => {
+  const resposne = await axiosInstance.post(`/api/teams/${teamId}`, {
+    accept: isAccept,
+  });
   return resposne.data;
-}
+};
 
 // 특정 팀 정보 조회
 export const getTeamInfo = async (teamId: number) => {
   const response = await axiosInstance.get(`/api/teams/${teamId}`);
   return response.data;
-}
+};
 
 // 팀원 수정 요청
 export const ModifyTeamMembers = async (teamId: number, payload: any) => {
-  console.log('payload 확인:', payload);
+  console.log("payload 확인:", payload);
   const response = await axiosInstance.patch(`api/teams/${teamId}`, payload);
   return response.data;
-}
+};
