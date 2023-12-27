a8520674071b8aa4aee2f949d5a2d05e8e3eacb9
KangYunJi <dbswl701@ajou.ac.kr>
2023-12-27 11:08:12 +0900

    fix: 에디터 초기값 수정

--- idearly/src/pages/AlgorithmSolvingPage/components/AlgorithmEditor/AlgorithmEditor.tsx
+++ idearly/src/pages/AlgorithmSolvingPage/components/AlgorithmEditor/AlgorithmEditor.tsx
@@ -10,8 +10,8 @@ import { AlgorithmFooter } from "..";
 import * as S from "./AlgorithmEditor.styles";
 import { useExcuteTestMutation, useRunMutation } from "../../../../hooks";
 import { AlgorithmSubmitResult, AlgorithmTestResult } from "../AlgorithmResult";
-// import { algorithmProblemsAtom } from "../../../../store/Algorithm.atoms";
-// import { useAtomValue } from "jotai";
+import { algorithmProblemsAtom } from "../../../../store/Algorithm.atoms";
+import { useAtomValue } from "jotai";
 
 interface Prop {
   competitionId: string | undefined;
@@ -25,7 +25,7 @@ export const AlgorithmEditor = ({ competitionId, problemId, teamId }: Prop) => {
   const editorParentRef = useRef<HTMLDivElement>(null);
   const viewRef = useRef<EditorView | undefined>();
   const docRef = useRef<typeof yorkie.Document | undefined>();
-  // const problemData = useAtomValue(algorithmProblemsAtom);
+  const problemData = useAtomValue(algorithmProblemsAtom);
 
   const { mutate: executeMutate } = useExcuteTestMutation();
   const { mutate: runMutate } = useRunMutation();
@@ -74,12 +74,16 @@ export const AlgorithmEditor = ({ competitionId, problemId, teamId }: Prop) => {
       if (!root.content) {
         root.content = new yorkie.Text();
         // 하나하나 돌면서 값을 넣어준다.
-        // let newContent = problemData.code;
-        // newContent.split("").map((code, index) => {
-        //   doc.update((root: any) => {
-        //     root.content.edit(index, index + 1, code);
-        //   }, `update content byA ${client!.getID()}`);
-        // });
+        let newContent = problemData.code;
+
+        newContent.split("").map((code, index) => {
+          console.log(
+            `code 하나씩 출력: from: ${index}, to: ${index}, code: ${code}`
+          );
+          doc.update((root: any) => {
+            root.content.edit(index, index, code);
+          }, `update content byA ${client!.getID()}`);
+        });
       }
     }, "create content if not exists");
 
