7299cf5a9538741da3a0425836ab7d58fada2673
KangYunJi <dbswl701@ajou.ac.kr>
2023-12-27 09:32:34 +0900

    remove: 이전 대회 조회 페이지 삭제

--- idearly/src/pages/MyPage/MyPage.tsx
+++ idearly/src/pages/MyPage/MyPage.tsx
@@ -1,24 +1,31 @@
 import { useParams } from "react-router-dom";
-import { MyPageCurrentTeam, MyPageModifyInfo, MyPageNav, MyPagePrevCompe, MyPageWithdrawal } from "."
+import {
+  MyPageCurrentTeam,
+  MyPageModifyInfo,
+  MyPageNav,
+  MyPageWithdrawal,
+} from ".";
 import * as S from "./MyPage.styles";
 
 export const MyPage = () => {
   const { path } = useParams();
-  console.log(path);
 
   const renderMyPageContent = () => {
-    switch(path) {
-      case 'previousCompetition': return <MyPagePrevCompe /> 
-      case 'currentTeam': return <MyPageCurrentTeam /> 
-      case 'modifyMyInfo': return <MyPageModifyInfo /> 
-      case 'withdrawal': return <MyPageWithdrawal /> 
-      default: return <MyPageCurrentTeam /> 
+    switch (path) {
+      case "currentTeam":
+        return <MyPageCurrentTeam />;
+      case "modifyMyInfo":
+        return <MyPageModifyInfo />;
+      case "withdrawal":
+        return <MyPageWithdrawal />;
+      default:
+        return <MyPageCurrentTeam />;
     }
-  }
+  };
   return (
     <S.MyPageContainer>
       <MyPageNav />
       {renderMyPageContent()}
     </S.MyPageContainer>
-  )
-}
+  );
+};
